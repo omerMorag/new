1cca4e29b48b740b365c4a582b6b6f6f34526327
Omer Morag <omermorag2@gmail.com>
2021-10-25 21:22:23 +0300

    mm

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,17 @@ import {Users} from './pages/Users.jsx'
 import {UserDetails} from './pages/UserDetails.jsx'
 
 const routes = [
-    {
+  {
+    path: "/user/",
+  component:  UserDetails,
+  label: "UserDetails",
+}, 
+  {
       path: "/",
       component: Users,
       label: "Users",
     },
-    {
-        path: "/user/",
-      component:  UserDetails,
-      label: "UserDetails",
-    }
+
   ];
   
   export default routes;
